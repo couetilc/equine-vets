4e5757f3ae76c38dfa2d5a8684eecf85cc386b39
Connor Couetil <couetilc@gmail.com>
2018-07-09 14:38:49 -0400

    updates

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const e = React.createElement;
+
+class Message extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { msg: "BEST VETS" };
+    }
+    render() {
+        return e(
+
+        );
+    }
+}
+
+const domContainer = document.querySelector('#app');
+ReactDOM.render(, domContainer);
